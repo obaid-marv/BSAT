e6593c60864c341c5d7c1005de0e06004b787ed8
obaid-marv <84005194+obaid-marv@users.noreply.github.com>
2024-02-02 19:53:58 +0500

    miles to go before i sleep

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,13 @@ import Navbar from "../components/Navbar";
 
 import envelopeIcon from "../assets/email.svg";
 import coverimg from "../assets/mainimg.png"
-
+import group1 from "../assets/group1.svg"
+import group2 from "../assets/group2.svg"
+import group3 from "../assets/group3.svg"
+import group4 from "../assets/group4.svg"
+import group5 from "../assets/group5.svg"
+import group6 from "../assets/group6.svg"
+import arrowright from "../assets/arrowright.svg"
 
 const Home = () => {
 
@@ -41,6 +47,141 @@ const Home = () => {
                     <img src={coverimg} alt="COVER IMG" className="-mt-4 h-full w-full object-cover ml-14"/>
 
                 </div>
+            </div>
+
+            <div>
+                <p className="text-4xl font-bold">Full Services to Grow Your Business </p>
+            </div>
+
+            <div className="container max-w-screen-md mt-10 mb-6">
+                <p className="text-base font-medium text-customgrey">Rely on seasoned professionals for tailored SEO advice and strategic planning aligned with your unique goals</p>
+            </div>
+
+            <div className="container mx-auto flex flex-wrap flex-row items-start max-w-screen-md h-max">
+                <div className="container group flex flex-col justify-around h-96 w-56 m-4 p-5 bg-white rounded-lg shadow-custom transition-all duration-400 hover:bg-gradient-tr hover:scale-105">
+                    <div className="w-28 h-28 bg-white rounded-full flex items-center justify-center shadow-custom">
+                        <img
+                            src={group1}
+                            alt="Your Alt Text"
+                            className="w-16 h-16"
+                        />
+                    </div>
+                    <p className="text-md font-bold group-hover:text-white">KEYWORD RESEARCH</p>
+                    <p className="text-xs font-medium leading-5 text-customgrey group-hover:text-white">Rely on seasoned professionals for tailored SEO advice and strategic planning aligned with your unique goals</p>
+                    <div className="container flex w-full items-center justify-between">
+                        <p className="text-sm text-custompurple group-hover:text-white">VIEW MORE</p>
+                        <img
+                            src={arrowright}
+                            alt="Your Alt Text"
+                            className="w-3 h-3 mr-16 rounded-full group-hover:text-white"
+                        />
+                    </div>
+                </div>
+
+                <div className="container group flex flex-col justify-around h-96 w-56 m-4 p-5 bg-white rounded-lg shadow-custom transition-all duration-400 hover:bg-gradient-tr hover:scale-105">
+                    <div className="w-28 h-28 bg-white rounded-full flex items-center justify-center shadow-custom">
+                        <img
+                            src={group2}
+                            alt="Your Alt Text"
+                            className="w-16 h-16"
+                        />
+                    </div>
+                    <p className="text-md font-bold group-hover:text-white">UNIQUE CONTENT </p>
+                    <p className="text-xs font-medium leading-5 text-customgrey group-hover:text-white">Rely on seasoned professionals for tailored SEO advice and strategic planning aligned with your unique goals</p>
+                    <div className="container flex w-full items-center justify-between">
+                        <p className="text-sm text-custompurple group-hover:text-white">VIEW MORE</p>
+                        <img
+                            src={arrowright}
+                            alt="Your Alt Text"
+                            className="w-3 h-3 mr-16 rounded-full group-hover:text-white"
+                        />
+                    </div>
+                </div>
+
+                <div className="container group flex flex-col justify-around h-96 w-56 m-4 p-5 bg-white rounded-lg shadow-custom transition-all duration-400 hover:bg-gradient-tr hover:scale-105">
+                    <div className="w-28 h-28 bg-white rounded-full flex items-center justify-center shadow-custom">
+                        <img
+                            src={group3}
+                            alt="Your Alt Text"
+                            className="w-16 h-16"
+                        />
+                    </div>
+                    <p className="text-md font-bold group-hover:text-white">CITATIONS SUGGESTIONS</p>
+                    <p className="text-xs font-medium leading-5 text-customgrey group-hover:text-white">Rely on seasoned professionals for tailored SEO advice and strategic planning aligned with your unique goals</p>
+                    <div className="container flex w-full items-center justify-between">
+                        <p className="text-sm text-custompurple group-hover:text-white">VIEW MORE</p>
+                        <img
+                            src={arrowright}
+                            alt="Your Alt Text"
+                            className="w-3 h-3 mr-16 rounded-full group-hover:text-white"
+                        />
+                    </div>
+                </div>
+
+                <div className="container group flex flex-col justify-around h-96 w-56 m-4 p-5 bg-white rounded-lg shadow-custom transition-all duration-400 hover:bg-gradient-tr hover:scale-105">
+                    <div className="w-28 h-28 bg-white rounded-full flex items-center justify-center shadow-custom">
+                        <img
+                            src={group4}
+                            alt="Your Alt Text"
+                            className="w-16 h-16"
+                        />
+                    </div>
+                    <p className="text-md font-bold group-hover:text-white">AUDIT REPORT</p>
+                    <p className="text-xs font-medium leading-5 text-customgrey group-hover:text-white">Rely on seasoned professionals for tailored SEO advice and strategic planning aligned with your unique goals</p>
+                    <div className="container flex w-full items-center justify-between">
+                        <p className="text-sm text-custompurple group-hover:text-white">VIEW MORE</p>
+                        <img
+                            src={arrowright}
+                            alt="Your Alt Text"
+                            className="w-3 h-3 mr-16 rounded-full group-hover:text-white"
+                        />
+                    </div>
+                </div>
+
+                <div className="container group flex flex-col justify-around h-96 w-56 m-4 p-5 bg-white rounded-lg shadow-custom transition-all duration-400 hover:bg-gradient-tr hover:scale-105">
+                    <div className="w-28 h-28 bg-white rounded-full flex items-center justify-center shadow-custom">
+                        <img
+                            src={group5}
+                            alt="Your Alt Text"
+                            className="w-16 h-16"
+                        />
+                    </div>
+                    <p className="text-md font-bold group-hover:text-white">WEBSITE ANALYTICS</p>
+                    <p className="text-xs font-medium leading-5 text-customgrey group-hover:text-white">Rely on seasoned professionals for tailored SEO advice and strategic planning aligned with your unique goals</p>
+                    <div className="container flex w-full items-center justify-between">
+                        <p className="text-sm text-custompurple group-hover:text-white">VIEW MORE</p>
+                        <img
+                            src={arrowright}
+                            alt="Your Alt Text"
+                            className="w-3 h-3 mr-16 rounded-full group-hover:text-white"
+                        />
+                    </div>
+                </div>
+
+                <div className="container group flex flex-col justify-around h-96 w-56 m-4 p-5 bg-white rounded-lg shadow-custom transition-all duration-400 hover:bg-gradient-tr hover:scale-105">
+                    <div className="w-28 h-28 bg-white rounded-full flex items-center justify-center shadow-custom">
+                        <img
+                            src={group6}
+                            alt="Your Alt Text"
+                            className="w-16 h-16"
+                        />
+                    </div>
+                    <p className="text-md font-bold group-hover:text-white">PAYMENT MANAGEMENT</p>
+                    <p className="text-xs font-medium leading-5 text-customgrey group-hover:text-white">Rely on seasoned professionals for tailored SEO advice and strategic planning aligned with your unique goals</p>
+                    <div className="container flex w-full items-center justify-between">
+                        <p className="text-sm text-custompurple group-hover:text-white">VIEW MORE</p>
+                        <img
+                            src={arrowright}
+                            alt="Your Alt Text"
+                            className="w-3 h-3 mr-16 rounded-full group-hover:text-white"
+                        />
+                    </div>
+                </div>
+
+
+                
+                
+
             </div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,16 @@ export default {
       textColor: {
         'custom': '#008080',
         'customgrey': '#575757',
-        
+        'custompurple': '#7660EE'
       },
       fontFamily: {
         'sans': ['Inter', 'sans-serif'],
+      },
+      boxShadow:{
+        custom:'rgba(0, 0, 0, 0.1) 2px 4px 12px'
+      },
+      backgroundImage: {
+        'gradient-tr': 'linear-gradient(to top right, #353438, #008080)',
       },
     },
   },
